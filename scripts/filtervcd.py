ebbf2bcd4544d4d883fc72938a4c1f290748c80e
Sjoerd T. Timmer <sjoerd.timmer@gmail.com>
2018-07-12 11:00:18 +0200

    add small script that makes vcd pulseview compatible

---
+++ scripts/filtervcd.py
@@ -0,0 +1,23 @@
+#!/bin/env python3
+import sys
+
+bad_signals = []
+
+with open(sys.argv[1], "r") as f:
+	for line in f:
+		if line.startswith("$var"):
+			try:
+				if int(line.split()[2]) != 1:
+					bad_signals.append(line.split()[3])
+					#print("bad signals is now: {}".format(bad_signals),file=sys.stderr)
+					continue
+			except:
+				pass
+
+		isbad = False
+		for s in bad_signals:
+			if line.strip().endswith(s):
+				isbad = True
+				break
+		if not isbad:
+			print(line, end="")
