795f1dd420b02f6826ab57d5b5b7a6331f465865
Roel Jordans <r.jordans@tue.nl>
2018-08-21 11:15:33 +0200

    Adding pin constraints and cleaning up unused code parts

--- rtl/top.vhd
+++ rtl/top.vhd
@@ -18,6 +18,7 @@ entity top is
     dataIn : in std_logic_vector (g_ADC_DRIVER_BITS-1 downto 0);
     dataOvIn : in std_logic;
     clk : in std_logic;
+    clk_uart : std_logic;
     rst : in std_logic;
     trigger : in std_logic;
     i_start_transfer : in std_logic;
@@ -39,7 +40,6 @@ architecture behaviour of top is
 
   signal buffer_write_en : std_logic;
   signal clk_intern : std_logic;
-  signal clk_uart : std_logic;
   signal write_address : std_logic_vector(g_BUFFER_INDEXSIZE-1 downto 0);
   signal read_address : std_logic_vector(g_BUFFER_INDEXSIZE-1 downto 0);
   signal read_set_address : std_logic_vector(g_BUFFER_INDEXSIZE-1 downto 0);
@@ -79,17 +79,6 @@ architecture behaviour of top is
       o_q: out std_logic_vector(g_BUFFER_INDEXSIZE-1 downto 0)
     );
   end component;
-
-  --component settable_counter
-  --  generic ( g_SIZE : natural );
-  --  port (
-  --    i_en     : in std_logic;
-  --    i_clk    : in std_logic;
-  --    i_set    : in std_logic;
-  --    i_data   : in std_logic_vector(g_BUFFER_INDEXSIZE-1 downto 0);
-  --    o_data   : out std_logic_vector(g_BUFFER_INDEXSIZE-1 downto 0)
-  --  );
-  --end component;
 
   component uart_expander
     generic (g_WORDSIZE: natural; g_WORDCOUNT : natural);
@@ -131,16 +120,7 @@ architecture behaviour of top is
     );
   end component;
 
-  component clock_divider
-    generic (g_SIZE: natural);
-    port (
-      i_clk     : in std_logic := '0';
-      o_clk     : out std_logic := '0';
-      i_ratio   : in std_logic_vector(g_SIZE-1 downto 0)
-    );
-   end component;
 begin
-
   adc_input_bus <= dataOvIn & dataIn;
 
 adc_driver_1 : adc_driver
@@ -162,22 +142,13 @@ write_index_counter : simple_counter
     i_clk => clk_intern,
     o_q => write_address);
 
---read_index_counter : settable_counter
---  generic map (g_SIZE => g_BUFFER_INDEXSIZE)
---  port map (
---    i_clk => clk_intern,
---  o_data => read_address,
---  i_en => '1',
---  i_set => '0',
--- i_data => read_set_address);
-
 data_buffer_1 : data_buffer
   generic map (g_ADDRESS_WIDTH => g_BUFFER_INDEXSIZE, g_DATA_WIDTH => c_STORAGE_WIDTH)
   port map (
     i_wclk => clk_intern,
     i_we => buffer_write_en,
     i_waddr => write_address,
-    i_rclk => clk_intern,
+    i_rclk => clk_uart,
     i_re => '1',
     i_raddr => read_address,
     i_wdata => adc_data_selected,
@@ -197,10 +168,6 @@ uart_1 : uart_expander
     o_ready     => uart_ready
     );
 
-
-
---  o_led <= data_output_bus(0);
-
 write_controller_1 : write_controller
   generic map (g_ADDRESS_BITS => g_BUFFER_INDEXSIZE, g_START_OFFSET => 2047)
   port map (
@@ -210,7 +177,7 @@ write_controller_1 : write_controller
     i_arm => write_arm,
     o_write_en => buffer_write_en,
     o_start_addr => read_set_address,
-    o_trigger_done => open
+    o_trigger_done => write_trigger_done
     );
 
   readout_controller_1 : readout_controller
@@ -221,19 +188,11 @@ write_controller_1 : write_controller
       i_start_addr   => read_set_address,
       o_arm          => write_arm,
       o_read_enable  => open,
-      o_read_addr    => open,
+      o_read_addr    => read_address,
       i_word_ready   => uart_ready,
       o_tx_enable    => uart_dataready,
       o_tx_ready     => open,
       i_tx_start     => i_start_transfer
    );
-
-  clock_divider_1 : clock_divider
-    generic map (g_SIZE => c_CLOCK_SIZE)
-    port map (
-      i_clk => clk_intern,
-      o_clk => clk_uart,
-      i_ratio => std_logic_vector(to_unsigned(c_CLOCK_DIVIDER, c_CLOCK_SIZE))
-    );
 
 end;
