665f6d082551a676700060198e4b364e80870273
Roel Jordans <r.jordans@tue.nl>
2018-09-26 16:32:39 +0200

    Updating plot script with some extra comments

--- scripts/dump_uart.py
+++ scripts/dump_uart.py
@@ -6,21 +6,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 
-plt.ion()
-
 dev          = serial.Serial()
-dev.port     = '/dev/ttyUSB1'
+dev.port     = '/dev/ttyUSB0'
 dev.baudrate = 1000000
 dev.timeout  = 1
 dev.bytesize = 7
 dev.dtr      = 1
 dev.open()
 
+##
+# Detect if we're running in interactive mode to avoid problems with matplotlib
+##
+def in_ipython():
+    try:
+        __IPYTHON__
+    except NameError:
+        return False
+    else:
+        return True
+##
+# Trigger the digitizer using the DTR line on the UART.  The DTR line is active low so writing a 0 here gives a 1 on the physical line.
+##
 def trigger():
     dev.dtr = 0
     #time.sleep(0.001)
     dev.dtr = 1
 
+##
+# Convert a single sample value (as 2 byte pair) into a signed integer representation
+##
 def val_from_raw(raw1, raw0):
   val_unsigned = (raw1 << 7) + raw0
   if val_unsigned > 2047:
@@ -28,7 +42,12 @@ def val_from_raw(raw1, raw0):
   else:
     return val_unsigned
 
-def read_sample():
+##
+# Trigger the digitizer and read the resulting set of samples from the UART.  Return a list of samples for each channel separately.
+#
+# Usage: (ch1, ch2) = read_samples()
+##
+def read_samples():
     time.sleep(2)
     dev.reset_input_buffer()
     trigger()
@@ -37,24 +56,22 @@ def read_sample():
 
     for b in range(2048):
         raw = dev.read(4)
-        #print(raw[2])
-        #print(raw[3])
-        #print(raw[2])
-        #print(raw[3])
-        #print("%04d: % 3d % 3d % 3d % 3d"%(b,raw[0], raw[1], raw[2], raw[3]))
         ch1 = val_from_raw(raw[1], raw[0])
         ch2 = val_from_raw(raw[3], raw[2])
-        print("% 4d: % 4d % 4d"%(b, ch1,ch2))
+        #print("% 4d: % 4d % 4d"%(b, ch1,ch2))
         ch1_data.append(ch1)
         ch2_data.append(ch2)
 
 
     print("done")
-    dev.timeout = 1
-    junk = dev.read(10000)
-    print("%d bytes remained in buffer after read"%len(junk))
+    #dev.timeout = 1
+    #junk = dev.read(10000)
+    #print("%d bytes remained in buffer after read"%len(junk))
     return (ch1_data, ch2_data)
 
+##
+# Compute the FFT of a sample sequence and plot it using matplotlib.
+##
 def fft_from_samples(data):
     # Scale data to voltages
     y = np.asarray(data) / 2048. * 1.75/2.0;
@@ -68,52 +85,82 @@ def fft_from_samples(data):
 
     fig, ax = plt.subplots()
     ax.plot(x, y)
+    ax.set_xlabel("time (us)")
+    ax.set_ylabel("input level (V)")
 
-    # Apply windowing function (choose from hamming, hann, bartlett, blackman)
+    # Apply windowing function (7-term Blackman-Harris)
     # Compute FFT, default size = data size
-    window = 'blackmanharris-7'
-    if(window == 'blackmanharris-7'):
-        w = np.zeros((N), dtype='float')
-        for n in np.arange(0, N):
-            w[n] = 0.27105140069342 - 0.43329793923448 * np.cos(   2.*np.pi*n/N) + 0.21812299954311 * np.cos(2.*2.*np.pi*n/N) - 0.06592544638803 * np.cos(3.*2.*np.pi*n/N) + 0.01081174209837 * np.cos(4.*2.*np.pi*n/N) - 0.00077658482522 * np.cos(5.*2.*np.pi*n/N) + 0.00001388721735 * np.cos(6.*2.*np.pi*n/N)
+    w = np.zeros((N), dtype='float')
+    for n in np.arange(0, N):
+        w[n] = 0.27105140069342 \
+             - 0.43329793923448 * np.cos(   2.*np.pi*n/N) \
+             + 0.21812299954311 * np.cos(2.*2.*np.pi*n/N) \
+             - 0.06592544638803 * np.cos(3.*2.*np.pi*n/N) \
+             + 0.01081174209837 * np.cos(4.*2.*np.pi*n/N) \
+             - 0.00077658482522 * np.cos(5.*2.*np.pi*n/N) \
+             + 0.00001388721735 * np.cos(6.*2.*np.pi*n/N)
 
     # Coherent gain
     CPG = np.sum(w)/N
     CPGdB = -20*np.log10(CPG)
-    
+
     # Compute FFT with windowing applied
     # Divide by N to compensate for FFT scaling
     yf = np.fft.fft(y*w) / N
-    
+
     # Translate to power dBm in 50R, fold spectrum to single band, and convert into dB
     ypowl = 2*1000*np.abs(yf[:N//2])**2/50
     ypow = 10*np.log10(ypowl)
-    
-    # Locate peak value
-    peak = np.argmax(ypow)
-    
+
     # Start plotting things
     fig, ax = plt.subplots()
     ax.plot(xf, ypow+CPGdB)
-    ax.plot(xf[peak], ypow[peak]+CPGdB, "ro")
     ax.set_ylabel("power (dBm)")
     ax.set_xlabel("frequency (MHz)")
-    
+
+    # Locate peak value
+    peak = np.argmax(ypow)
+    ax.plot(xf[peak], ypow[peak]+CPGdB, "ro")
+
     # Plot label with statistics for peak
     # Compute equivalent noise bandwidth for our window and determine SINAD to add to labels
     ENBW = N * np.sum(w**2)/np.sum(w)**2
     ENBWdB = 10*np.log10(ENBW)
-    
+
     # Processing gain
     PGdB = 10*np.log10(2.0/N)
-    
-    print("GPG={3:.2f}dB, ENBW={0:.2f}dB, PG={1:.2f}dB, N={2}".format(ENBWdB, PGdB, N, CPGdB))
-    peak_width=20
-    
-    NF = 10*np.log10(np.sum(ypowl[:peak-peak_width])+np.sum(ypowl[peak+peak_width:]))
-    NFtrue = NF + CPGdB + PGdB - ENBWdB
-    SINAD = ypow[peak]-NFtrue
+
+    # TODO fix the computations here...
+    #print("GPG={3:.2f}dB, ENBW={0:.2f}dB, PG={1:.2f}dB, N={2}".format(ENBWdB, PGdB, N, CPGdB))
+    #peak_width=20
+
+    #NF = 10*np.log10(np.sum(ypowl[:peak-peak_width])+np.sum(ypowl[peak+peak_width:]))
+    #NFtrue = NF + CPGdB + PGdB - ENBWdB
+    #SINAD = ypow[peak]-NFtrue
+
+    # Place labels
+    #plt.annotate(
+    #  "{0:.2f}dBm @ {1:.2f}MHz (bin {5})\nNF={3:.2f}dBm, SINAD={2:.2f}dB\nENOB={4:.2f}bit".format(ypow[peak]+CPGdB, xf[peak], SINAD, NFtrue, (SINAD-1.76)/6.02, peak),
+    #  xy=(xf[peak],ypow[peak]+CPGdB),
+    #  xytext=(220,-20),
+    #  textcoords='offset points',
+    #  ha='right', va='bottom',
+    #  bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+    #  arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
 
 
-(ch1, ch2) = read_sample()
+##
+# Example run code
+##
+(ch1, ch2) = read_samples()
 fft_from_samples(ch1)
+
+##
+# Fix plotting in interactive mode...
+if in_ipython():
+    plt.ion()
+
+##
+# Show results
+plt.show()
+
