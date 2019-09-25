12d44293e49977e74438555ea36079797d3d8681
Sjoerd T. Timmer <sjoerd.timmer@gmail.com>
2019-09-25 12:07:03 +0200

    change how tri-state sa/scl are handled

--- rtl/housekeeping/i2c2.vhd
+++ rtl/housekeeping/i2c2.vhd
@@ -21,8 +21,8 @@ entity i2c2 is
     o_next      : out std_logic := '0';
     
     -- i2c interface
-    sda	  : inout std_logic := 'Z';
-    scl	  : inout std_logic := 'Z'
+    sda	  : inout std_logic;
+    scl	  : inout std_logic
     );
 end  i2c2;
 
@@ -36,9 +36,19 @@ architecture behave of i2c2 is
   signal r_rw   : std_logic;
 
   signal test_count : std_logic_vector(num_bits(8) downto 0);
-    
+
+  signal r_sda : std_logic := '1';
+  signal r_scl : std_logic := '1';
+
+  
 begin
+
+  scl <= 'Z' when r_scl = '1' else '0';
+  sda <= 'Z' when r_sda = '1' else '0';
+  
   test_count <= std_logic_vector(to_unsigned(r_count, test_count'length));
+
+  
   process(i_clk) is
   begin
     if rising_edge(i_clk) then
@@ -49,53 +59,53 @@ begin
             r_data <= i_data;
             r_rw <= i_rw;
             r_count <= 0;
-            sda <= '0';
+            r_sda <= '0';
             r_state <= s_Addr;
           else
-            sda <= 'Z';
+            r_sda <= '1';
           end if;
         when s_Addr =>
           if scl = '0' then
-            scl <= 'Z';
+            r_scl <= '1';
             r_count <= r_count + 1;
             if r_count = g_ADDR'length-1 then
               r_State <= s_Rw;
             end if;
           else
-            scl <= '0';
+            r_scl <= '0';
             if g_ADDR(g_ADDR'length-r_count-1) = '1' then
-              sda <= 'Z';
+              r_sda <= '1';
             else
-              sda <= '0';
+              r_sda <= '0';
             end if;
           end if;
         when s_Rw =>
           if scl = '0' then
-            scl <= 'Z';
+            r_scl <= '1';
             r_state <= s_AddrAck;
           else
-            scl <= '0';
+            r_scl <= '0';
             if r_rw = '0' then
-              sda <= '0';
+              r_sda <= '0';
             else
-              sda <= 'Z';
+              r_sda <= '1';
             end if;
           end if;
         when s_AddrAck =>
           if scl = '0' then
-            scl <= 'Z';
+            r_scl <= '1';
             r_state <= s_Data;
             r_count <= 0;
-            if sda /= '0' then
+            if sda = '1' then
               -- TODO: raise error
             end if;
           else
-            scl <= '0';
-            sda <= 'Z';
+            r_scl <= '0';
+            r_sda <= '1';
           end if;
         when s_Data =>
           if scl = '0' then
-            scl <= 'Z';
+            r_scl <= '1';
             r_count <= (r_count + 1) ;
             if r_count = 6 then
               o_next <= '1';
@@ -112,19 +122,19 @@ begin
               end if;
             end if;
           else
-            scl <= '0';
+            r_scl <= '0';
             --report "r_count: " & integer'image(r_count);
             --report "index: " & integer'image(to_integer(unsigned(i_numbytes)) * 8 - r_count - 1);
             if r_rw = '1' or r_data(7 - r_count) = '1' then
-              sda <= 'Z';
+              r_sda <= '1';
             else
-              sda <= '0';
+              r_sda <= '0';
             end if;
           end if;
         when s_DataAck =>
           o_datavalid <= '0';
           if scl = '0' then
-            scl <= 'Z';
+            r_scl <= '1';
             if i_valid = '1' and  i_restart = '0' then
               -- continue with next byte
               r_state <= s_Data;
@@ -136,34 +146,34 @@ begin
               -- there may be data after the restart in which case we need to
               -- release sda now. otherwise we pull low to make sure the stop
               -- is after the last clock rise.
-              if i_valid = '1' then
-                sda <= 'Z';
-              else
-                sda <= '0';
-              end if;
+              --if i_valid = '1' then
+              --  r_sda <= '1';
+              --else
+              --  r_sda <= '0';
+              --end if;
             end if;
             -- check that there is ack
-            if sda /= '0' then
+            if sda = '1' then
               -- TODO: raise error
             end if;
           else
-            scl <= '0';
+            r_scl <= '0';
             if r_rw = '1' then
-              sda <= '0'; -- transmit ack
+              r_sda <= '0'; -- transmit ack
             else
-              sda <= 'Z'; -- accept ack bit
+              r_sda <= '1'; -- accept ack bit
             end if;
           end if;
         when s_Stop =>
-          if scl /= '0' then
-            scl <= '0';
+          if i_valid = '1' then
+            r_sda <= '1';
           else
-            scl <= 'Z';
-            if i_valid = '1' then
-              sda <= 'Z';
-            else
-              sda <= '0';
-            end if;
+            r_sda <= '0';
+          end if;
+          if scl = '1' then
+            r_scl <= '0';
+          else
+            r_scl <= '1';
             r_state <= s_Idle;
           end if;
       end case;
